1fab157fb6101dfb1946162158ae354f20be8f1a
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-02-20 20:26:16 +0300

    (unless, else)

--- Ruby/Первая_лаба/third_task.rb
+++ Ruby/Первая_лаба/third_task.rb
@@ -1,13 +1,8 @@
 puts 'Какой ваш любимый язык?'
 lang = gets.chomp.downcase 
 
-if lang == 'ruby'
-	puts 'Подлиза'
+unless lang == 'ruby'
+	puts 'Скоро будет Ruby'
 else
-	case lang 
-	when 'c++'
-		puts 'C++ - это хорошо, но Ruby лучше'
-	when 'python'
-		puts 'Неженка, переходи на Ruby'
-	end
+	puts 'Подлиза'
 end
