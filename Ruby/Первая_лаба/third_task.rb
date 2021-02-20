d065ddf0c5fb82b2a952556f1f7bd04a766f166e
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-02-20 20:31:38 +0300

    (Ternary operator)

--- Ruby/Первая_лаба/third_task.rb
+++ Ruby/Первая_лаба/third_task.rb
@@ -1,10 +1,4 @@
 puts 'Какой ваш любимый язык?'
 lang = gets.chomp.downcase 
 
-if lang == 'ruby'
-	puts 'Подлиза'
-elsif lang == '1c'
-	puts 'Ясно...'
-else
-	puts 'Скоро будет Ruby'
-end
+lang == 'ruby' ? puts('Подлиза'): puts('Скоро будет Ruby')
