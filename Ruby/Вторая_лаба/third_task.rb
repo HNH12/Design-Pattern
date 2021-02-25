7125329145fa06fe346e411a9a1a44aaeb5e1c53
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-02-25 19:56:24 +0300

    Third_method

--- Ruby/Вторая_лаба/third_task.rb
+++ Ruby/Вторая_лаба/third_task.rb
@@ -1,12 +1,14 @@
 def read_array(arr, count_el) 
 	for i in 0..count_el-1
-		arr[i] = $stdin.gets.to_i
+		el = $stdin.gets.to_i
+		arr += [el]		
 	end
+	arr
 end
 
 
 count_el = ARGV[0].to_i
 
 arr = []
-read_array(arr, count_el)
+arr = read_array(arr, count_el)
 print arr
