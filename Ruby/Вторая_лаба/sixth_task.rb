ef23f47544f783f9b031422a5a70c9303f2f400c
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-02-25 21:59:02 +0300

    33_issue

--- Ruby/Вторая_лаба/sixth_task.rb
+++ Ruby/Вторая_лаба/sixth_task.rb
@@ -11,28 +11,16 @@ def read_file(path)
 end
 
 
-def get_elements_after_max(arr)
-	index_max = 0
-	max = arr[0]
-	for i in 0..arr.size-1
-		if arr[i] > max
-			puts i
-			index_max = i
-			max = arr[i]
+def is_alternating?(arr)
+	for i in 1..arr.size-1
+		if (arr[i] > 0 && arr[i-1] > 0) || (arr[i] < 0 && arr[i-1] < 0)
+			return false
 		end
 	end
-	index_max
+	true
 end
 
 
 path = "file.txt"
 arr = read_file(path)
-
-
-print arr
-index_first_max_elem = get_elements_after_max(arr)+1
-if  index_first_max_elem < arr.size
-	print arr[index_first_max_elem..]
-else
-	print []
-end
+puts is_alternating?(arr)
