817e1ec9031d2e70cb50c529e956ce7a12a1aa83
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-02-27 16:04:12 +0300

    Update tenth_task.rb

--- Ruby/Вторая_лаба/tenth_task.rb
+++ Ruby/Вторая_лаба/tenth_task.rb
@@ -1,4 +1,4 @@
 str = gets.chomp
 
-reg = /([1-3]{1}[1-9]{1}|[1-9]{1}) (december|january|february|march|april|may|june|july|august|september|october|november) ([1-9]{1}\d*)/
+reg = /([1-9]{0}[1-3]{1}[1-9]{1}\s|[1-9]{0}[1-9]{1}\s) (december|january|february|march|april|may|june|july|august|september|october|november) ([1-9]{1}\d*)/
 print str.scan(reg)
