78834a8cbf9d5f4ca69e6a6c857234609453bdf6
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-03-01 00:19:17 +0300

    Update tenth_task.rb

--- Ruby/Первая_лаба/tenth_task.rb
+++ Ruby/Первая_лаба/tenth_task.rb
@@ -1,18 +1,29 @@
-months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-day = 2
-sunday_count = 0
-
-for year in 1901..2001
-	for month in months
-		day += month
-		if year % 4 == 0 && month == 28 && year % 100 != 0 || year % 400 == 0
-                day += 1
-        end
-        if day % 7 == 0
-            sunday_count += 1
-        end
+def get_possible_solutions(perimeter)
+	possible_perimeters = 0
+	for side_a in 1..perimeter/2
+		for side_b in side_a+1..perimeter/2
+			side_c = perimeter - side_a - side_b
+			if side_a**2 + side_b**2 == side_c**2
+				possible_perimeters += 1
+			end
+		end
 	end
+	possible_perimeters
 end
 
-print sunday_count
+
+def get_max_possible_solutions()
+	max_possible_solutions = 0
+	max_perimeter = 0
+	for perimeter in 1..1001
+		current_possible_solutions = get_possible_solutions(perimeter)
+		if current_possible_solutions > max_possible_solutions
+			max_possible_solutions = current_possible_solutions
+			max_perimeter = perimeter
+		end
+	end
+	max_perimeter
+end
+
+
+print get_max_possible_solutions
