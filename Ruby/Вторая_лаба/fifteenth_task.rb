6546d6615c71aebebf0f24fae737b62d0517b0c2
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-02-28 15:09:33 +0300

    5_issue

--- Ruby/Вторая_лаба/fifteenth_task.rb
+++ Ruby/Вторая_лаба/fifteenth_task.rb
@@ -16,8 +16,15 @@ def get_most_frequent_character_in_string(str)
 end
 
 
+def get_frequency_in_text(lines, char)
+	str_lines = lines.join("")
+
+	return str_lines.count(char).to_f/str_lines.size.to_f
+end
+
+
 def get_difference_by_task(lines, str)
-	str_lines = lines.join("\n")
+	str_lines = lines.join("")
 
 	most_frequent_char_in_str, char_frequecy_in_str = get_most_frequent_character_in_string(str)
 
@@ -26,12 +33,25 @@ def get_difference_by_task(lines, str)
 end
 
 
-def sort_by_task(lines)
-	print lines.sort { |first_str, second_str| get_difference_by_task(lines, first_str) <=> get_difference_by_task(lines, second_str)}	
+def get_average_frequency(lines)
+	arr_char = lines.join("").split("").uniq
+	arr_frequency = arr_char.map {|char| get_frequency_in_text(lines, char)}
+	return (arr_frequency.sum/arr_frequency.size).round(3)
+end
+
+
+def get_square_deviation(lines, str)
+	most_frequent_char, char_frequecy_in_str = get_most_frequent_character_in_string(str)
+	return Math.sqrt((char_frequecy_in_str - get_average_frequency(lines)).abs)
+end
+
+
+def sort_by_task_5(lines)
+	print lines.sort { |first_str, second_str| get_square_deviation(lines, first_str) <=> get_square_deviation(lines, second_str)}	
 end
 
 
 path = "file.txt"
 lines = read_file(path)
 
-sort_by_task(lines)
+sort_by_task_5(lines)
