25d19e5ecc71359fb3ce6f6aad4aab5c6e04f76d
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-03-14 11:27:57 +0300

    Четвертое задание

--- Ruby/Третья_лаба/tasks.rb
+++ Ruby/Третья_лаба/tasks.rb
@@ -1,28 +1,39 @@
 class Employee
-	attr_accessor :name, :birthday, :address, 
-				  :e_mail, :passport_series, :passport_number, :specialty, :work_experience
+	attr_accessor :name, :birthday, :address, :passport_series, :passport_number, :specialty, :work_experience
 
 
-	def is_rus_number(number)
+	def is_rus_number?(number)
 		(number =~ /\+7-[0-9]{3}-[0-9]{7}|8[0-9]{10}|\+7-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}/) != nil ? true : false
 	end	
 
 	def get_rus_number(number)
 		begin
-			is_rus_number(number) ? @phone_number =  number.gsub(/(^8)(\d{3})(\d{7})/, '+7-\2-\3'): raise
+			is_rus_number?(number) ? @phone_number =  number.gsub(/(^8)(\d{3})(\d{7})/, '+7-\2-\3'): raise
+		rescue
+			puts "Неверный тип аргумента"
+		end
+	end
+
+	def is_email?(email)
+		(email =~ /[A-z]{3,}[1-9]*[A-z]*@(gmail|mail|edu)(.com|.ru|.kubsu.ru)/) != nil ? true : false
+	end
+
+	def get_email_downcase(email)
+		begin
+			is_email?(email) ? @email = email.downcase : raise
 		rescue
 			puts "Неверный тип аргумента"
 		end
 	end
 
 	def initialize(name, birthday, phone_number, address, 
-				  e_mail, passport, specialty, work_experience, 
+				  email, passport, specialty, work_experience, 
 				  previous_work = nil, previous_post = nil, previous_salary = nil)
 		@name = name
 		@birthday = birthday
-		@phone_number = phone_number
+		@phone_number = get_rus_number phone_number
 		@address = address
-		@e_mail = e_mail
+		@email = email
 		@passport = passport
 		@specialty = specialty
 		@work_experience = work_experience
@@ -38,8 +49,16 @@ class Employee
 		@phone_number
 	end
 
-	def phone_number=(x)
-		@phone_number = get_rus_number x
+	def phone_number=(phone_number)
+		@phone_number = get_rus_number phone_number
+	end
+
+	def email
+		@email
+	end
+
+	def email=(email)
+		@email = get_email_downcase email
 	end
 
 	def previous_work
@@ -74,5 +93,6 @@ second_emp = Employee.new("Толстиков Илья Вадимович", "22.
 	"0555 239999", "Programmer", 0)
 
 
-first_emp.phone_number = '89183616209'
-puts first_emp.phone_number
+first_emp.email = "Henuh12@edu.kubsu.ru"
+
+puts first_emp.email
