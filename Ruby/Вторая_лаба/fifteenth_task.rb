92ef24e970b7ac78320f810b58b63f4861af9b93
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-03-22 09:55:59 +0300

    Все задания

--- Ruby/Вторая_лаба/fifteenth_task.rb
+++ Ruby/Вторая_лаба/fifteenth_task.rb
@@ -9,6 +9,49 @@ def read_file(path)
 end
 
 
+
+def get_most_frequent_character_in_string(str)
+	arr = str.split('')
+	most_frequent_char = arr.max { |a| arr.count(a).to_f/arr.size.to_f }
+	return most_frequent_char, arr.count(most_frequent_char).to_f/arr.size.to_f
+end
+
+
+def get_difference_by_task(lines, str)
+	str_lines = lines.join("\n")
+
+	most_frequent_char_in_str, char_frequecy_in_str = get_most_frequent_character_in_string(str)
+
+	char_frequency_in_lines = str_lines.count(most_frequent_char_in_str).to_f/str_lines.size.to_f
+	return (char_frequency_in_lines - char_frequecy_in_str).abs
+end
+
+
+def sort_by_task_3(lines)
+	print lines.sort { |first_str, second_str| get_difference_by_task(lines, first_str) <=> get_difference_by_task(lines, second_str)}	
+end
+
+
+
+def get_frequency_in_text(lines, char)
+	str_lines = lines.join("")
+
+	return str_lines.count(char).to_f/str_lines.size.to_f
+end
+
+
+def get_square_deviation(lines, str)
+	most_frequent_char, char_frequecy_in_str = get_most_frequent_character_in_string str
+	return Math.sqrt((char_frequecy_in_str - get_frequency_in_text(lines, most_frequent_char)).abs)
+end
+
+
+def sort_by_task_5(lines)
+	print lines.sort { |first_str, second_str| get_square_deviation(lines, first_str) <=> get_square_deviation(lines, second_str)}	
+end
+
+
+
 def get_most_ASCII_character_in_string(str)
 	arr = str.split('')
 	most_frequent_char = arr.max { |a| a.ord }
@@ -44,6 +87,7 @@ end
 def sort_by_task_9(lines)
 	print lines.sort { |first_str, second_str| get_square_deviation_9_issue(first_str) <=> get_square_deviation_9_issue(second_str)}	
 end
+
 
 
 path = "file.txt"
