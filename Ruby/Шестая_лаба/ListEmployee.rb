250b77c3480af369bab5671fc5aee0d61cabe092
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-04-30 09:47:48 +0300

    8 задание (полностью)

--- Ruby/Шестая_лаба/ListEmployee.rb
+++ Ruby/Шестая_лаба/ListEmployee.rb
@@ -1,6 +1,8 @@
 require_relative 'Employee.rb'
 require 'mysql2'
 require 'openssl'
+require 'yaml'
+require 'json'
 
 
 class ListEmployee
@@ -52,15 +54,9 @@ class ListEmployee
 		end
 	end
 
-	def read_list_DB(client)
-		results = client.query("SELECT * FROM employees")
-		list_emp = ListEmployee.new()
-		results.each do |row|
-			@new_list.append(Employee.new(row['Name'], row['Birthday'].to_s, row['PhoneNumber'], row['Address'],
-								 row['Email'], row['Passport'].to_s, row['Specialty'], row['WorkExperience'],
-								 row['PreviousWork'], row['PreviousPost'], row['PreviousSalary']))
-		end
-		list_emp
+	def read_list_DB()
+		@new_list = DB_work.DB_connection.read_db
+
 	end
 
 	def to_s
@@ -69,66 +65,16 @@ class ListEmployee
 		str
 	end
 
-	def add_to_DB(client, emp)
-		emp_previous_work = emp.previous_work == 'Поле не указано' ? nil : emp.previous_work
-		emp_previous_post = emp.previous_post == 'Поле не указано' ? nil : emp.previous_post
-		emp_previous_salary = emp.previous_salary == 'Поле не указано' ? nil : emp.previous_salary
-
-		if emp_previous_salary == nil || emp_previous_post == nil || emp_previous_work == nil
-			client.query("INSERT INTO `stuff`.`employees` (`Name` ,`Birthday` ,`PhoneNumber` ,`Address` ,`Email` ,
-														`Passport` ,`Specialty` ,`WorkExperience`)
-													VALUES ('#{emp.name}', '#{emp.birthday}', '#{emp.phone_number}', '#{emp.address}', '#{emp.email}',
-														'#{emp.passport}', '#{emp.specialty}', #{emp.work_experience.to_i})")
-		else
-			client.query("INSERT INTO `stuff`.`employees` (`Name` ,`Birthday` ,`PhoneNumber` ,`Address` ,`Email` ,
-														`Passport` ,`Specialty` ,`WorkExperience`, `PreviousWork` ,`PreviousPost` ,`PreviousSalary`)
-													VALUES ('#{emp.name}', '#{emp.birthday}', '#{emp.phone_number}', '#{emp.address}', '#{emp.email}',
-														'#{emp.passport}', '#{emp.specialty}', #{emp.work_experience.to_i},
-														'#{emp_previous_work}', '#{emp_previous_post}', #{emp_previous_salary})")
-		end
+	def add_to_DB(emp)
+		DB_work.DB_connection.add_to_DB(emp)
 	end
 
-	def change_node(client, all_emp)
-		all_emp.each { |emp|
-			emp_previous_work = emp.previous_work == 'Поле не указано' ? nil : emp.previous_work
-			emp_previous_post = emp.previous_post == 'Поле не указано' ? nil : emp.previous_post
-			emp_previous_salary = emp.previous_salary == 'Поле не указано' ? nil : emp.previous_salary
-			emp.name = 'Толстиков Илья Вадимович'
-			if emp_previous_salary == nil || emp_previous_post == nil || emp_previous_work == nil
-				client.query("UPDATE employees SET Name = '#{emp.name}'
-										WHERE Passport = '#{emp.passport}'")
-			else
-				client.query("UPDATE employees SET Name = '#{emp.name}'
-										WHERE Passport = '#{emp.passport}'")
-			end
-			# ,
-			# 	Birthday = '#{emp.birthday}',
-			# 		PhoneNumber = '#{emp.phone_number}',
-			# 		Address' = #{emp.address}',
-			# 	Email = '#{emp.email}',
-			# 		Passport = '#{emp.passport}',
-			# 		Specialty = '#{emp.specialty}',
-			# 		WorkExperience = `#{emp.work_experience.to_i}`,
-			# 		PreviousWork = '#{emp_previous_work}',
-			# 		PreviousPost = '#{emp_previous_post}',
-			# 		PreviousSalary = `#{emp_previous_salary}`,
-			# 		Birthday = '#{emp.birthday}',
-			# 		PhoneNumber = '#{emp.phone_number}',
-			# 		Address' = #{emp.address}',
-			# 	Email = '#{emp.email}',
-			# 		Passport = '#{emp.passport}',
-			# 		Specialty = '#{emp.specialty}',
-			# 		WorkExperience = `#{emp.work_experience.to_i}`,
-			# 		PreviousWork = '#{emp_previous_work}',
-			# 		PreviousPost = '#{emp_previous_post}',
-			# 		PreviousSalary = `#{emp_previous_salary}`
-		}
+	def change_node(all_emp)
+		DB_work.DB_connection.change_node all_emp
 	end
 
-	def delete_from_db(client, all_emp)
-		all_emp.each { |emp| emp
-			client.query("DELETE FROM employees WHERE Passport = '#{emp.passport}'")
-		}
+	def delete_from_db(all_emp)
+		DB_work.DB_connection.delete_from_db all_emp
 	end
 
 	def sort field
--- Ruby/Шестая_лаба/WorkWithDB.rb
+++ Ruby/Шестая_лаба/WorkWithDB.rb
@@ -2,19 +2,36 @@ require_relative 'Employee.rb'
 require 'mysql2'
 
 
-class WorkWithDB
-  def add_to_DB(client, emp)
+class DB_work
+  @@DB_connection = nil
+  # private_class_method :new
+
+  def initialize
+    @client = Mysql2::Client.new(
+      :host => 'localhost',
+      :username => 'root',
+      :database => 'stuff'
+    )
+  end
+
+  def self.DB_connection
+    if @@DB_connection == nil
+      @@DB_connection = DB_work.new
+    end
+  end
+
+  def add_to_DB(emp)
     emp_previous_work = emp.previous_work == 'Поле не указано' ? nil : emp.previous_work
     emp_previous_post = emp.previous_post == 'Поле не указано' ? nil : emp.previous_post
     emp_previous_salary = emp.previous_salary == 'Поле не указано' ? nil : emp.previous_salary
 
     if emp_previous_salary == nil || emp_previous_post == nil || emp_previous_work == nil
-      client.query("INSERT INTO `stuff`.`employees` (`Name` ,`Birthday` ,`PhoneNumber` ,`Address` ,`Email` ,
+      @client.query("INSERT INTO `stuff`.`employees` (`Name` ,`Birthday` ,`PhoneNumber` ,`Address` ,`Email` ,
 														`Passport` ,`Specialty` ,`WorkExperience`)
 													VALUES ('#{emp.name}', '#{emp.birthday}', '#{emp.phone_number}', '#{emp.address}', '#{emp.email}',
 														'#{emp.passport}', '#{emp.specialty}', #{emp.work_experience.to_i})")
     else
-      client.query("INSERT INTO `stuff`.`employees` (`Name` ,`Birthday` ,`PhoneNumber` ,`Address` ,`Email` ,
+      @client.query("INSERT INTO `stuff`.`employees` (`Name` ,`Birthday` ,`PhoneNumber` ,`Address` ,`Email` ,
 														`Passport` ,`Specialty` ,`WorkExperience`, `PreviousWork` ,`PreviousPost` ,`PreviousSalary`)
 													VALUES ('#{emp.name}', '#{emp.birthday}', '#{emp.phone_number}', '#{emp.address}', '#{emp.email}',
 														'#{emp.passport}', '#{emp.specialty}', #{emp.work_experience.to_i},
@@ -22,46 +39,38 @@ class WorkWithDB
     end
   end
 
-  def change_node(client, all_emp)
+  def change_node(all_emp)
     all_emp.each { |emp|
       emp_previous_work = emp.previous_work == 'Поле не указано' ? nil : emp.previous_work
       emp_previous_post = emp.previous_post == 'Поле не указано' ? nil : emp.previous_post
       emp_previous_salary = emp.previous_salary == 'Поле не указано' ? nil : emp.previous_salary
       emp.name = 'Толстиков Илья Вадимович'
       if emp_previous_salary == nil || emp_previous_post == nil || emp_previous_work == nil
-        client.query("UPDATE employees SET Name = '#{emp.name}'
+        @client.query("UPDATE employees SET Name = '#{emp.name}'
 										WHERE Passport = '#{emp.passport}'")
       else
-        client.query("UPDATE employees SET Name = '#{emp.name}'
+        @client.query("UPDATE employees SET Name = '#{emp.name}'
 										WHERE Passport = '#{emp.passport}'")
       end
-      # ,
-      # 	Birthday = '#{emp.birthday}',
-      # 		PhoneNumber = '#{emp.phone_number}',
-      # 		Address' = #{emp.address}',
-      # 	Email = '#{emp.email}',
-      # 		Passport = '#{emp.passport}',
-      # 		Specialty = '#{emp.specialty}',
-      # 		WorkExperience = `#{emp.work_experience.to_i}`,
-      # 		PreviousWork = '#{emp_previous_work}',
-      # 		PreviousPost = '#{emp_previous_post}',
-      # 		PreviousSalary = `#{emp_previous_salary}`,
-      # 		Birthday = '#{emp.birthday}',
-      # 		PhoneNumber = '#{emp.phone_number}',
-      # 		Address' = #{emp.address}',
-      # 	Email = '#{emp.email}',
-      # 		Passport = '#{emp.passport}',
-      # 		Specialty = '#{emp.specialty}',
-      # 		WorkExperience = `#{emp.work_experience.to_i}`,
-      # 		PreviousWork = '#{emp_previous_work}',
-      # 		PreviousPost = '#{emp_previous_post}',
-      # 		PreviousSalary = `#{emp_previous_salary}`
     }
   end
 
-  def delete_from_db(client, all_emp)
+  def delete_from_db(all_emp)
     all_emp.each { |emp| emp
-    client.query("DELETE FROM employees WHERE Passport = '#{emp.passport}'")
+      @client.query("DELETE FROM employees WHERE Passport = '#{emp.passport}'")
     }
   end
+
+  def read_db
+    new_list = []
+    @client.query("SELECT * FROM employees").each do |row|
+    	new_list.append(Employee.new(row['Name'], row['Birthday'].to_s, row['PhoneNumber'], row['Address'],
+    						 row['Email'], row['Passport'].to_s, row['Specialty'], row['WorkExperience'],
+    						 row['PreviousWork'], row['PreviousPost'], row['PreviousSalary']))
+    end
+    new_list
+  end
 end
+
+
+# DB_work.DB_connection.print_p
