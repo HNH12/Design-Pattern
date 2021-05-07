49084dfa249183106f9508167de06b5e5f6b84e3
HNH12 <62258989+HNH12@users.noreply.github.com>
2021-05-07 19:30:08 +0300

    6 задание

---
+++ Ruby/Седьмая_лаба/Department.rb
@@ -0,0 +1,35 @@
+require_relative 'Post_list'
+
+
+class Department
+  def initialize(department_name)
+    self.department_name = department_name
+  end
+
+  def department_name=(department_name)
+    @department_name = department_name
+  end
+
+  def department_name
+    @department_name
+  end
+
+  def post_list=(post_list)
+    @post_list = post_list
+  end
+
+  def post_list
+    @post_list
+  end
+end
+
+# test_dep = Department.new 'Отдел кадров'
+#
+# test_post_list = Post_list.new test_dep
+# test_post = Post.new('Программист', 40000, 10000, 3000, 50)
+#
+# test_post_list.add test_post
+#
+# test_dep.post_list =test_post_list
+#
+# puts test_dep.post_list.choose(0).get_full_info
--- Ruby/Седьмая_лаба/Post_list.rb
+++ Ruby/Седьмая_лаба/Post_list.rb
@@ -1,11 +1,6 @@
 require_relative 'Post'
 
 
-class Department
-
-end
-
-
 class Post_list
   attr_accessor :department, :post_list
 
@@ -19,7 +14,7 @@ class Post_list
   end
 
   def choose(numb)
-    self.post_list[numb].get_full_info
+    self.post_list[numb]
   end
 end
 
@@ -31,4 +26,4 @@ end
 #
 # test_post_list.add test_post
 #
-# puts test_post_list.choose(0)
+# puts test_post_list.choose(0).get_full_info
